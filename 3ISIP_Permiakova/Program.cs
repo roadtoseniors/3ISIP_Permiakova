2fd72eb81b9f189a72f44d43f5819646259bfca4
roadtoseniors <Mariapoyua@yandex.ru>
2025-10-16 15:18:31 +0300

    Декомпозиция :3

--- 3ISIP_Permiakova/Program.cs
+++ 3ISIP_Permiakova/Program.cs
@@ -1,1 +1,42 @@
-﻿
+﻿class Player
+{
+
+}
+class Enemy
+{
+
+}
+
+class Goblin : Enemy
+{
+
+}
+
+class Skelet : Enemy
+{
+
+}
+
+class Mag : Enemy
+{
+
+}
+
+class Goblin_boss : Goblin
+{
+
+}
+
+class Skelet_boss_kova : Skelet
+{
+
+}
+class Skelet_boss_pest : Skelet
+{
+
+}
+
+class Magc_boss : Mag
+{
+
+}
